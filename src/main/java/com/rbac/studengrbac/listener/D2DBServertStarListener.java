b9f4a7d9b68430daec6af6e70234979b3580a6df
yaolijie <1600199763@qq.com>
2019-06-05 11:13:32 +0800

    添加h2数据库依赖，添加数据库的监听程序

---
+++ src/main/java/com/rbac/studengrbac/listener/D2DBServertStarListener.java
@@ -0,0 +1,35 @@
+package com.rbac.studengrbac.listener;
+
+import org.h2.tools.Server;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import java.sql.SQLException;
+
+/**
+ * Created by Administrator on 2019/6/5.
+ */
+
+public class D2DBServertStarListener implements ServletContextListener {
+
+    private Server server;
+
+    @Override
+    public void contextInitialized(ServletContextEvent servletContextEvent) {
+        try{
+            System.out.println("正在启动h2 数据库。。。。。。。");
+            server=Server.createTcpServer().start();
+            System.out.println("h2 数据库启动成功。。。。。。。。。。。");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent servletContextEvent) {
+        if (this.server!=null){
+            this.server.stop();
+            this.server=null;
+        }
+    }
+}
